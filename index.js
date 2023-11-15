57e03a82ef95ffb6ba41908a5bcec1beecace46d
Andrew Nguyen <21520144@gm.uit.edu.vn>
2023-11-15 18:11:42 +0700

    LOLOL These files were outside the src folder, so there they are

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 require("dotenv").config();
-const web = require("./src/routes/web");
+const initApiRoutes = require("./src/routes/api");
+const configCors = require("./src/config/cors");
 const app = express();
+const bodyParser = require("body-parser");
 const port = process.env.PORT;
 
-app.use("/", web);
+//config CORS
+configCors(app);
+
+//config body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//routes
+initApiRoutes(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
