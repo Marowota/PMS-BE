3b8a5a2aa9b8b65c3e16b85f1d7424043455dcbd
Andrew Nguyen <21520144@gm.uit.edu.vn>
2023-11-23 23:35:17 +0700

    Add cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -5,6 +5,7 @@ const configCors = (app) => {
   app.use(function (req, res, next) {
     // Website you wish to allow to connect
     res.setHeader("Access-Control-Allow-Origin", process.env.REACT_URL);
+    res.setHeader("Access-Control-Allow-Origin", process.env.TEST_URL);
 
     // Request methods you wish to allow
     res.setHeader(
